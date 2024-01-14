58f5f241c2b646aa78f27c7f0c769c04b84d2bf4
ashishthakur <156094188+thakur040@users.noreply.github.com>
2024-01-14 14:08:27 +0530

    Add files via upload

---
+++ AssignementLoopquestionnumber4.java
@@ -0,0 +1,10 @@
+package loops;
+
+import java.util.Scanner;
+
+public class AssignementLoopquestionnumber4 {
+    public static void main(String[] args) {
+        Scanner Sc=new Scanner(System.in);
+
+    }
+}
